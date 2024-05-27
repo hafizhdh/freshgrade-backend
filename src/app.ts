a0e37dbd3ce71f14bc057a309e87d11a9a7c4028
Hafiz <hafizadhyaulhaq16@gmail.com>
2024-05-27 12:22:09 +0700

    handles if endpoint not found

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(express.json())
 app.use(routes)
 
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Endpoint not found' });
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
